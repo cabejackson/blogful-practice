9665af91612749e00cdfd60a2edb6a43e6c0f666
Caleb Jackson <jacksoncaleb.calebjackson@gmail.com>
2020-10-21 10:39:45 -0400

    set-up + started GET tests

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
+const ArticlesService = require('./articles-service');
+
 
 const app = express();
 
@@ -11,9 +13,23 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
 app.use(helmet());
+
+app.get("/articles", (req, res, next) => {
+  // res.send("All articles")
+  // ArticlesService.getAllArticles(/* some knex instance would go here */)
+  const knexInstance = req.app.get('db')
+  ArticlesService.getAllArticles(knexInstance)
+    .then(articles => {
+      res.json(articles)
+    })
+    .catch(next)
+})
+
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
+
 app.use(function errorHandler(error, req, res, next) {
   let response;
   if (NODE_ENV === "production") {
